1518100a3bfa133ac083c54e44542c58b3801517
쑤언 탕_Xuan Thang <thangdx@hivelab.com>
2025-12-29 15:56:02 +0700

    Hero #1

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+// const nav = $('#nav')
+const header = $("#header");
+
+// load nội dung dùng chung
+fetch("../header.html")
+  .then((res) => res.text())
+  .then((html) => {
+    header.innerHTML = html;
+
+    const btnMenu = $(".btn_menu");
+    if (btnMenu) {
+      btnMenu.onclick = function () {
+        header.classList.toggle("is_open");
+      };
+    }
+  });
+
+function handleResize(element, className, breakpoint = 768) {
+  if (!element) return;
+
+  const isDesktop = window.innerWidth > breakpoint;
+  if (isDesktop && element.classList.contains(className)) {
+    element.classList.remove(className);
+  }
+}
+
+window.addEventListener("resize", () => {
+  handleResize(header, "is_open");
+});
